20d48778c625befd227d14193d9f12519af45cf4
Pierrick <mail@pierrickmarie.info>
2024-01-09 15:30:10 +0100

    Images pour electron

--- images/electron/build.sh
+++ images/electron/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker rmi pimarie/electron:latest
-docker build -t pimarie/electron .
+docker build -t pimarie/electron:2024-01-08 .
--- images/electron/run.sh
+++ images/electron/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v`pwd`/src:/app/src --rm -it electron-wrapper bash
+docker run --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v`pwd`/:/app/src --rm -it pimarie/electron:latest bash
